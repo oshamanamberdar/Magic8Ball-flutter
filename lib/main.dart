08ae0d3e854f5dc00866e26fbf8bb17161b79252
Oshama Nmaberdar <oshamanamberdargujjar@gmail.com>
2021-06-15 01:59:11 +0545

    This is my first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,47 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Center(
+            child: Text('Guessing Ball'),
+          ),
+        ),
+        body: guessingBall(),
+      ),
+    ),
+  );
+}
+
+class guessingBall extends StatefulWidget {
+  @override
+  _guessPageState createState() => _guessPageState();
+}
+
+class _guessPageState extends State<guessingBall> {
+  int guessNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            // ignore: deprecated_member_use
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  guessNumber = Random().nextInt(5) + 1;
+                });
+              },
+              child: Image.asset('images/ball$guessNumber.png'),
+            ),
+          )
+        ],
       ),
     );
+  }
+}
